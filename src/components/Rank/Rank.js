75de5d89ff7464fa9a1314bf769d729e9b95767f
Amith <amithpasham@gmail.com>
2021-06-04 13:10:24 +0530

    neww

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -5,13 +5,13 @@ import React from 'react';
 const Rank = () => {
   return(
     <div>
-      <div className="white f3">
+      {/* <div className="white f3">
         {"Tirth, your current Rank is..."}
       </div>
       <div className="white f1">
         {"#2"}
       </div>
-      
+       */}
       
     </div>
   )
